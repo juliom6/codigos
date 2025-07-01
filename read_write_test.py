12fd34304f510434dfdaabf074eff924d3960d89
juliom6 <juliom6@gmail.com>
2025-07-01 10:25:07 -0500

    Update read_write_test.py changing file test

--- read_write_test.py
+++ read_write_test.py
@@ -5,25 +5,32 @@ NUM_COLUMNS = 10 # 510 limit
 NUM_ROWS = (10**5)
 
 ###
+# SECRET_ACCESS_KEY = ""
+# STORAGE_NAME = "xyz123456789"
+# spark.sparkContext._jsc.hadoopConfiguration().set(
+#     f"fs.azure.account.key.{STORAGE_NAME}.dfs.core.windows.net", SECRET_ACCESS_KEY
+# )
+# root_path = "abfss://testcontainer@xyz123456789.dfs.core.windows.net"
+###
 df = spark.range(1, NUM_ROWS + 1)
 new_columns = ["col" + ("000" + str(l))[-3:] for l in range(NUM_COLUMNS)]
 for col_name in new_columns:
     df = df.withColumn(col_name, F.rand(seed=0))
 
 df.write.mode("overwrite").parquet(
-    root_path + "/desa/bcp/rdv/test2/"
+    root_path + "/bronze/test2/"
 )
 print("Write ok")
 print(datetime.now())
 
 df1 = spark.read.parquet(
-    root_path + "/desa/bcp/rdv/test2/"
+    root_path + "/bronze/test2/"
 )
 df2 = spark.read.parquet(
-    root_path + "/desa/bcp/rdv/test2/"
+    root_path + "/bronze/test2/"
 )
 df_result = df1.join(df2, df1.id + df2.id == (NUM_ROWS + 1)).select(df1["*"])
 df_result.write.mode("overwrite").parquet(
-    root_path + "/desa/bcp/rdv/test3/" # abfss://testcontainer@xyz123456789.dfs.core.windows.net
+    root_path + "/bronze/test3/"
 )
 print(datetime.now())
