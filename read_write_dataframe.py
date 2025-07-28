99a77667b968fabbbfda3c7e10f9c8c7e4249eb1
julio delgado <juliom6@gmail.com>
2025-07-28 10:58:00 -0500

    added new read/write script

---
+++ read_write_dataframe.py
@@ -0,0 +1,17 @@
+##### read dataframe twice
+df1 = spark.read.parquet(
+    root_path + "/bronze/parquet_files/"
+)
+
+df2 = spark.read.parquet(
+    root_path + "/bronze/parquet_files/"
+)
+
+NUM_ROWS = df1.count()
+
+df_result = df1.join(df2, df1.id + df2.id == (NUM_ROWS + 1)).select(df1["*"])
+df_result.write.mode("overwrite").parquet(
+    root_path + "/silver/datatool/"
+)
+
+df_result.limit(10).show()
