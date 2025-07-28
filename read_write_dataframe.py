fce02a48b5e33410e354fc48e14b71ff004d6964
juliom6 <juliom6@gmail.com>
2025-07-28 11:25:58 -0500

    Update read_write_dataframe.py

--- read_write_dataframe.py
+++ read_write_dataframe.py
@@ -7,6 +7,7 @@ df2 = spark.read.parquet(
     root_path + "/bronze/parquet_files/"
 )
 
+##### make join and write result
 NUM_ROWS = df1.count()
 
 df_result = df1.join(df2, df1.id + df2.id == (NUM_ROWS + 1)).select(df1["*"])
