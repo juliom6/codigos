bb1bdc2e012c6833cd647fecc7f2fbd0cbc4ff75
juliom6 <juliom6@gmail.com>
2025-07-28 10:48:05 -0500

    Update read_write_test.py

--- read_write_test.py
+++ read_write_test.py
@@ -1,8 +1,4 @@
 from pyspark.sql import functions as F
-from datetime import datetime
-
-NUM_COLUMNS = 3
-NUM_ROWS = (10**3) * 2
 
 ###
 # SECRET_ACCESS_KEY = ""
@@ -13,18 +9,20 @@ NUM_ROWS = (10**3) * 2
 # root_path = "abfss://testcontainer@xyz123456789.dfs.core.windows.net"
 ###
 
+NUM_COLS = 3
+NUM_ROWS = (10**3) * 2
+
 df = spark.range(1, NUM_ROWS + 1)
-new_columns = ["col" + ("000" + str(l))[-3:] for l in range(NUM_COLUMNS)]
+new_columns = ["col" + ("000" + str(l))[-3:] for l in range(NUM_COLS)]
 seed = 0
 for col_name in new_columns:
     df = df.withColumn(col_name, F.rand(seed=seed))
     seed += 1
 
-print(datetime.now())
 df.write.mode("overwrite").parquet(
     root_path + "/bronze/test1/"
 )
-print(datetime.now())
+
 df1 = spark.read.parquet(
     root_path + "/bronze/test1/"
 )
@@ -35,7 +33,7 @@ df_result = df1.join(df2, df1.id + df2.id == (NUM_ROWS + 1)).select(df1["*"])
 df_result.write.mode("overwrite").parquet(
     root_path + "/bronze/test2/"
 )
-print(datetime.now())
+
 df_result.limit(10).show()
 
 
