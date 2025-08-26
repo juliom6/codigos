3438054bafbdc883f5e3577b03dfe36c64d9ad92
juliom6 <juliom6@gmail.com>
2025-08-26 10:43:15 -0500

    Update read_write_dataframe.py added delta write

--- read_write_dataframe.py
+++ read_write_dataframe.py
@@ -1,4 +1,4 @@
-##### read dataframe twice
+# read dataframe twice
 df1 = spark.read.parquet(
     root_path + "/bronze/parquet_files/"
 )
@@ -7,12 +7,18 @@ df2 = spark.read.parquet(
     root_path + "/bronze/parquet_files/"
 )
 
-##### make join and write result
+# make a simple join
 NUM_ROWS = df1.count()
-
 df_result = df1.join(df2, df1.id + df2.id == (NUM_ROWS + 1)).select(df1["*"])
+
+# write result in parquet format
 df_result.write.mode("overwrite").parquet(
-    root_path + "/silver/datatool/"
+    root_path + "/silver/datatool/parquet/"
+)
+
+# write result in delta format
+df_result.write.format("delta").mode("overwrite").save(
+    root_path + "/silver/datatool/delta/"
 )
 
 df_result.limit(10).show()
