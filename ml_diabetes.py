c89aad5a0b9d0ae52bf5f966fc9d760efacc03e3
juliom6 <juliom6@gmail.com>
2025-08-29 17:30:38 -0500

    Update ml_diabetes.py

--- ml_diabetes.py
+++ ml_diabetes.py
@@ -2,6 +2,7 @@ df = spark.read.csv(
     root_path + "/machine_learning/diabetes/diabetes.csv", header=True, inferSchema=True
 )
 
+# Prepara la data y lo separa en dataset de entrenamiento y test
 from pyspark.ml.feature import VectorAssembler
 
 assembler = VectorAssembler(
@@ -18,23 +19,21 @@ assembler = VectorAssembler(
     outputCol="features",
 )
 output_data = assembler.transform(df)
-
-from pyspark.ml.classification import LogisticRegression
-
 final_data = output_data.select("features", "Outcome")
+train, test = final_data.randomSplit([0.75, 0.25])
 
-train, test = final_data.randomSplit([0.7, 0.3])
+# Aplica Regresion Logistica y genera un modelo
+from pyspark.ml.classification import LogisticRegression
 models = LogisticRegression(labelCol="Outcome")
 model = models.fit(train)
+model.summary.predictions.describe().show()
 
-summary = model.summary
+# 
+predict = model.evaluate(test)
+predict.predictions.show(10)
 
-summary.predictions.describe().show()
-
+# Evalua la eficiencia de las predicciones
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
-
-predictions = model.evaluate(test)
-predictions.predictions.show(20)
 
 evaluator = BinaryClassificationEvaluator(
     rawPredictionCol="rawPrediction", labelCol="Outcome"
